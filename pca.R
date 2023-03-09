d3e7e38c29a382fb3f37b2bc785743bb63547eaf
Leonard Jones <leonard.jones@gmail.com>
2023-03-09 16:47:43 -0500

    Update pca.R

--- pca.R
+++ pca.R
@@ -11,29 +11,37 @@ libraries("adegenet","RColorBrewer","parallel","RColorBrewer", "ggplot2")
 locality.info <- read.csv("sample_summary.csv",stringsAsFactors = T)
 head(locality.info)
 
-# check that all samples in vcf and sample info are represented
-all(colnames(gen.total.vcf@gt)[-1]== locality.info$sample) 
-
 total.vcf <- vcfR::read.vcfR("~/Documents/sirtalis/stacks/ele_map/sirtalis/p1r80/populations.snps.vcf")
+# check that all samples in vcf and sample info are represented
+all(colnames(total.vcf@gt)[-1]== locality.info$sample) 
 total.gl <- vcfR::vcfR2genlight(total.vcf)
 ploidy(total.gl) <- 2
 pop(total.gl) <- locality.info$population
 
 z.vcf <- vcfR::read.vcfR("~/Documents/sirtalis/stacks/ele_map/sirtalis/z_p1r80/populations.snps.vcf")
+# check that all samples in vcf and sample info are represented
+all(colnames(z.vcf@gt)[-1]== locality.info$sample) 
 z.gl <- vcfR::vcfR2genlight(z.vcf)
 ploidy(z.gl) <- 2
 pop(z.gl) <- locality.info$population
 
 autosomes.vcf <- vcfR::read.vcfR("~/Documents/sirtalis/stacks/ele_map/sirtalis/autosomes_p1r80/populations.snps.vcf")
+# check that all samples in vcf and sample info are represented
+all(colnames(autosomes.vcf@gt)[-1]== locality.info$sample) 
 autosomes.gl <- vcfR::vcfR2genlight(autosomes.vcf)
 ploidy(autosomes.gl) <- 2
 pop(autosomes.gl) <- locality.info$population
 
+
 # div colors  "#2C7BB6", "#FDAE61", "#D7191C", "#ABD9E9"
 # K = 4 pop order      E          C           S         W
-div.colors <- c("#2C7BB6", "#FDAE61", "#D7191C", "#ABD9E9")
+# div.colors <- c("#2C7BB6", "#FDAE61", "#D7191C", "#ABD9E9")
+# colors incorrectly assigned in figure
+div.colors <- c("#FDAE61", "#2C7BB6", "#D7191C", "#ABD9E9")
 
-# TOTAL PCA
+#############
+# TOTAL PCA #
+#############
 
 total.pca <- glPca(total.gl, n.cores = 8, nf = 3, parallel = TRUE)
 total.pca.scores <- as.data.frame(total.pca$scores)
@@ -50,7 +58,9 @@ p <- p + theme_bw()
 p <- p + xlab("PC1 (12.11%)") + ylab("PC2 (9.48%)")
 p
 
-# Z CHROMOSOME PCA
+####################
+# Z CHROMOSOME PCA #
+####################
 
 z.pca <- glPca(z.gl, n.cores = 8, nf = 3, parallel = TRUE)
 z.pca.scores <- as.data.frame(z.pca$scores)
@@ -66,7 +76,9 @@ p <- p + theme_bw()
 p <- p + xlab("PC1 (1.86%)") + ylab("PC2 (0.97%)")
 p
 
-# AUTOSOME PCA
+################
+# AUTOSOME PCA #
+################
 
 autosomes.pca <- glPca(autosomes.gl, n.cores = 8, nf = 3, parallel = TRUE)
 autosomes.pca.scores <- as.data.frame(autosomes.pca$scores)
