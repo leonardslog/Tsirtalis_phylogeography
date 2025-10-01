510fc85691b52f5701d8da13245dbf10cb1a3638
Leonard Jones <leonard.jones@gmail.com>
2025-10-01 14:19:14 -0500

    Update model15.py

--- momi2/model15.py
+++ momi2/model15.py
@@ -4,7 +4,9 @@ import matplotlib
 import logging
 from autograd.numpy import log
 
-# null model, tree structure with no migration
+# tree structure with bidirectional southeast/central and central/east migration, 
+# and unidirectional west into central and east into southeast migration events 
+# with central and east population size changes
 
 logging.basicConfig(level=logging.INFO,
                     filename="model15.log")
